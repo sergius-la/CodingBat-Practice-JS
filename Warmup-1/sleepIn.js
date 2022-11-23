a9e1305b6947eb1648fffcd6d31022e8078e203c
S.L <sergius.la@gmail.com>
2022-11-23 07:47:20 -0800

    First commit

---
+++ Warmup-1/sleepIn.js
@@ -0,0 +1,17 @@
+/*
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+
+function sleepIn(weekday, vacation) {
+    // ⌨️ Your code
+}
+
+console.assert(sleepIn(false, false) === true, "Test #1 failed");
+console.assert(sleepIn(true, false) === false, "Test #2 failed");
+console.assert(sleepIn(false, true) === false, "Test #3 failed");
